13c10ad328e098b0292ae6b77f35a31165dd98cf
Andreas Maier <amaier@particle.uni-karlsruhe.de>
2010-11-28 19:57:03 +0100

    Some LaTeX functions with special treatment

--- Printer/Latex_out.pm
+++ Printer/Latex_out.pm
@@ -8,6 +8,16 @@ use Operator;
 #----------------------------------------------------------------------------------------------------
 use base 'Printer';
 
+sub init{
+    my $self=shift;
+    $self->SUPER::init;
+    %{$self->{specials}}=(
+	'/' => \&ratio_as_frac,
+	'*' => \&product,
+	'^' => \&power
+	);
+}
+
 sub number_to_string{
     return $_[1];
 }
@@ -20,13 +30,87 @@ sub string_to_string{
     return $_[1];
 }
 
+#special latex functions
+sub bracket_to_string{
+    my $self=shift;
+    my $brackets=shift;
+    my $args=shift;
+    if(scalar @$args > 1){
+	my $function=shift @$args;
+	given($function->name){
+	    when ('**subscript**'){return $self->subscript($args)}
+	    default {unshift @$args,$function}
+	}
+    }
+    return $self->SUPER::bracket_to_string($brackets,$args);
+}
+
+
 #format ratios as \frac{}{}
 sub ratio_as_frac{
     my $self=shift;
     shift;
     my $args=shift;
-    my $string='\frac{'.$self->to_string($$args[0]).'}{'.$self->to_string($$args[1]).'}';
+    my $num=$self->to_string($self->fall_through_bracket($$args[0]));
+    my $den=$self->to_string($self->fall_through_bracket($$args[1]));
+    my $string="\\frac{$num}{$den}";
     return $string;
+}
+
+#format products
+sub product{
+    my $self=shift;
+    shift;
+    my $args=shift;
+    #TODO: it would be wise to use operator_to_string here, but first these operators have to be defined
+    if($$args[0]->is eq 'number' and $$args[1]->is eq 'number'){
+	return $self->to_string($$args[0]).'\cdot'.$self->to_string($$args[1]) 
+    }
+    return $self->to_string($$args[0]).'\*'.$self->to_string($$args[1]) 
+}
+
+#format power (= remove superfluous brackets)
+sub power{
+    my $self=shift;
+    my $op=shift;
+    my $args=shift;
+    my $last_prec=shift;
+    #dd $$args[1];
+    my $real_args=$self->fall_through_bracket($$args[1]);
+    my $arg_str=$self->to_string($real_args);
+    if(length $arg_str==1){
+	return $self->to_string($$args[0])."^$arg_str";
+    }
+    else{
+	return $self->to_string($$args[0])."^{$arg_str}";
+    }
+}
+
+
+#format subscripts
+sub subscript{
+    my $self=shift;
+    my $args=shift;
+    # $args is a sequence, we have to go one level deeper
+    my $real_args=$$args[0]->args;
+    my $subscript=$self->to_string($$real_args[1]);
+    return $self->to_string($$real_args[0]).'_'.((length $subscript ==1 )?$subscript:"{$subscript}");
+}
+
+#remove unneeded bracket -> if $_[1] is a bracket (), return its argument
+sub fall_through_bracket{
+    my $self=shift;
+    my $object=shift;
+        if(
+	 ($object->is eq 'bracket')
+	 and ($object->name->[0] eq '(')
+	 and ($object->name->[1] eq ')')
+	and (scalar @{$object->args} !=2)	
+	){
+	# we have an unneeded bracket, let's jump one level deeper
+	return $object->args->[0];
+    }
+   return $object
 }
 
 1;
