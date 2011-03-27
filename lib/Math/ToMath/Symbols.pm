8ee9bf6ffb3feaa923d885d21e5cc4161906b6fe
Steffen Mueller <smueller@cpan.org>
2011-03-27 21:05:03 +0200

    Named symbols files and more docs

--- lib/Math/ToMath/Symbols.pm
+++ lib/Math/ToMath/Symbols.pm
@@ -32,9 +32,66 @@ None by default.
 
 Can export a shortcut name for this class: C<Symbols>
 
+=head1 METHODS
+
+=cut
+
+=head2 get_symbol_file_for_class
+
+Class method.
+
+Given a class name, returns the location of the symbols file for
+that class.
+
+Works for both parsers and printers by reducing the class name to
+the last namespace fragment:
+
+  my $symbols_file = Symbols->get_symbol_file_for_class("Math::ToMath::Parser::Latex");
+  my $symbols_file = Symbols->get_symbol_file_for_class("Math::ToMath::Printer::Latex");
+  # or simply:
+  my $symbols_file = Symbols->get_symbol_file_for_class("Latex");
+
+Dies on failure to locate the file.
+
+=cut
+
+sub get_symbol_file_for_class {
+    my $self  = shift;
+    my $class = shift;
+    # Match Foo in Bar::Foo or in Foo but not in BarFoo
+    $class =~ /(?:^|:)([^:]+)$/
+        or die "Cannot find symbol name for class '$class'";
+    my $symbol_file = File::Spec->catfile("Symbols", $1 . ".dat");
+    return dist_file('Math-ToMath', $symbol_file);
+}
+
+=head2 get_named_symbol_file
+
+Class method.
+
+Returns the location of a symbols file with the provided base name.
+
+  my $file = Symbols->get_named_symbol_file("Greek");
+  # /path/to/some/place/Symbols/Greek.dat
+
+=cut
+
+sub get_named_symbol_file {
+    my $class = shift;
+    my $basename = shift;
+    my $symbol_file = File::Spec->catfile("Symbols", $basename . ".dat");
+    return dist_file('Math-ToMath', $symbol_file);
+}
+
+1;
+
+__END__
+
 =head1 SEE ALSO
 
 L<Math::ToMath>
+
+L<File::ShareDir>
 
 =head1 AUTHOR
 
@@ -50,14 +107,3 @@ at your option, any later version of Perl 5 you may have available.
 
 =cut
 
-sub get_symbol_file_for_class {
-    my $self  = shift;
-    my $class = shift;
-    # Match Foo in Bar::Foo or in Foo but not in BarFoo
-    $class =~ /(?:^|:)([^:]+)$/
-        or die "Cannot find symbol name for class '$class'";
-    my $symbol_file = File::Spec->catfile("Symbols", $1 . ".dat");
-    return dist_file('Math-ToMath', $symbol_file);
-}
-
-1;
