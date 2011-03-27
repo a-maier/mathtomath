d04e532c5c5441bad94bf59702f579a25072b560
Steffen Mueller <smueller@cpan.org>
2011-03-27 17:11:23 +0200

    Introduce main module, get version from there.

---
+++ lib/Math/ToMath.pm
@@ -0,0 +1,54 @@
+package Math::ToMath;
+
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw();
+our @EXPORT_OK = qw( );
+our %EXPORT_TAGS = ( 'all' => \@EXPORT_OK );
+
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Math::ToMath - bla bla bla
+
+=head1 SYNOPSIS
+
+  use Math::ToMath;
+
+=head1 DESCRIPTION
+
+Stub documentation for Math::ToMath, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+=head1 SEE ALSO
+
+...
+
+=head1 AUTHOR
+
+Andreas Maier, E<lt>email@addressE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010-2011 by Andreas Maier
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
